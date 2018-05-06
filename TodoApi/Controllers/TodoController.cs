532a7c5ae8bdc50bed6543cfb4de09aeccd68443
Lucas Begnini <lucasbegnini@gmail.com>
2018-05-06 17:47:58 -0300

    Startup Project

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            Utils _utils = new Utils();
+            item.TimestampUpdate = _utils.GetTimestamp(System.DateTime.Now);
+            item.Content = _utils.DataToJson(item);
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            Utils _utils = new Utils();
+            todo.TimestampUpdate = _utils.GetTimestamp(System.DateTime.Now);
+            todo.Name = item.Name;
+
+            todo.Content = _utils.DataToJson(todo);
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
---
+++ TodoApi/Models/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TodoApi.Models
+{
+    public class Utils
+    {
+        public String GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyyMMddHHmmssffff");
+        }
+
+        public String DataToJson(TodoItem item)
+        {
+            TodoContent _content = new TodoContent
+            {
+                Name = item.Name,
+                TimestampUpdate = item.TimestampUpdate,
+                Id = item.Id
+            };
+
+            return JsonConvert.SerializeObject(_content);
+        }
+    }
+}

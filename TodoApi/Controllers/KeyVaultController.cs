b66274db838da7b46662a1f5dd0599b4e017d16a
Lucas Begnini <lucasbegnini@gmail.com>
2018-05-19 16:22:33 -0300

    use key vault to store secrets

--- TodoApi/Controllers/KeyVaultController.cs
+++ TodoApi/Controllers/KeyVaultController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
 using TodoApi.Models;
+using static TodoApi.Models.KeyVaultModel;
 
 namespace TodoApi.Controllers
 {
@@ -12,17 +14,42 @@ namespace TodoApi.Controllers
     [Route("api/keyvault")]
     public class KeyVaultController : Controller
     {
-        // /api/KeyVault - GET - All elements
-        [HttpGet]
-        public string GetToken()
+        // /api/KeyVault/{secretName} - GET - return a secret that is register with secret Name
+        [HttpGet("{secretName}", Name = "GetSecret")]
+        public string GetToken(string secretName)
         {
             KeyVaultModel kv = new KeyVaultModel();
             Task<string> task = Task.Run(async () =>
             {
-                return await kv.GetKeyVaultInfo();
+                return await kv.GetSecret(secretName);
             });
-            var result = task.Wait(10000);
             return task.Result;
+        }
+
+        // /api/KeyVault - POST - create new secret
+        [HttpPost()]
+        public string CreateSecret([FromBody] MyKeySecrets item)
+        {
+            KeyVaultModel kv = new KeyVaultModel();
+            Task task = Task.Run(() =>
+            {
+                kv.SetSecret(item.SecretName, item.SecretContent);
+            });
+
+            return "{ \"status\": \"OK\" }";
+        }
+
+        // /api/KeyVault - PUT - update secret
+        [HttpPut()]
+        public string UpdateSecret([FromBody] MyKeySecrets item)
+        {
+            KeyVaultModel kv = new KeyVaultModel();
+            Task task = Task.Run(() =>
+            {
+                kv.UpdateSecret(item.SecretName, item.SecretContent);
+            });
+
+            return "{ \"status\": \"OK\" }";
         }
     }
 }
--- TodoApi/Models/KeyVaultModel.cs
+++ TodoApi/Models/KeyVaultModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -10,47 +13,62 @@ namespace TodoApi.Models
 
     public class KeyVaultModel
     {
-        const string resourceGroupName = "MojioTestRG";
-        const string vaultName = "MojioTestKV";
-        const string apiVersion = "2016-10-01";
-        const string subscriptionId = "889e6a2d-569d-4786-be1c-6420a51ee9f8";
-        const string appId = "a9ce8b9b-86c9-4bc1-8839-3a0dc5b654b5";
-        const string uriId = "http://urimojiotest1EncryptApp";
-        const string directoryId = "common";
-        const string kvToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImlCakwxUmNxemhpeTRmcHhJeGRacW9oTTJZayIsImtpZCI6ImlCakwxUmNxemhpeTRmcHhJeGRacW9oTTJZayJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9jNmFlYTkxZS01ZDYzLTQ1NmYtODAwYS0zN2FmYmYzNjZhMTMvIiwiaWF0IjoxNTI2NDMzMDY0LCJuYmYiOjE1MjY0MzMwNjQsImV4cCI6MTUyNjQzNjk2NCwiYWNyIjoiMSIsImFpbyI6IlkyZGdZTEQwVHpBUzI3ZmxXY0VsL1NkQ2I0VGVoMjY1L0hCaXBkUDBtV1paM2lLdlpQTUIiLCJhbHRzZWNpZCI6IjE6bGl2ZS5jb206MDAwNjAwMDA4QzAxNjQ4NiIsImFtciI6WyJwd2QiXSwiYXBwaWQiOiI3ZjU5YTc3My0yZWFmLTQyOWMtYTA1OS01MGZjNWJiMjhiNDQiLCJhcHBpZGFjciI6IjIiLCJlX2V4cCI6MjYyODAwLCJlbWFpbCI6ImJlZ25pbmkxM0Bob3RtYWlsLmNvbSIsImZhbWlseV9uYW1lIjoiQmVnbmluaSIsImdpdmVuX25hbWUiOiJMdWNhcyIsImdyb3VwcyI6WyJlY2JjYmYyNC05NmZjLTQyNzQtOTMxMy00OWRhZjljNjM1ZDYiXSwiaWRwIjoibGl2ZS5jb20iLCJpcGFkZHIiOiIyMDEuODYuMzEuMTY5IiwibmFtZSI6Ikx1Y2FzIEJlZ25pbmkiLCJvaWQiOiI1Zjk3MjU1My1kNmRhLTRhMjEtOTM3NS1jODdhZjYzMGFlYzEiLCJwdWlkIjoiMTAwM0JGRkRBQTlCNzhDQyIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6InJ4dTRGWjlIVXRwUTJFTml4aTB1Q3ZhcnR5QnpXbS04bERZbnBGT0huZDQiLCJ0aWQiOiJjNmFlYTkxZS01ZDYzLTQ1NmYtODAwYS0zN2FmYmYzNjZhMTMiLCJ1bmlxdWVfbmFtZSI6ImxpdmUuY29tI2JlZ25pbmkxM0Bob3RtYWlsLmNvbSIsInV0aSI6ImgyNExJMVNlMlVDRXVDRF9BUTRBQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCJdfQ.kZc4pepvkvAGO-WlHilJvEwrnazLGXdXZM6P8zjon5M05nJwUBx61wEByZo-KSUrhrCBGVQxpmtrpkZpXQ5pEB2YngXY2urkYN1bPOwkG3nbMBWszomcy4dh8AbhG6QN6NV1a0-zk8q_utHfLdQxny3qWSoppP3gkSBNRBYLDrbWsX8pawQ3g9qbQttPOi3xNOCm-5yrbeQqwGZtcbb9wxEc7hmYgxda6MIFaLPBItrtIs-K9R0O82MmTk3gBQCXsq7SJUQM0MVya1a1myc53nFfgE030LfRW9R9rENAeBD39ohm9xg2u6bwt4wId-cvSupyFgSZB12zbQfkwye77g";
+        const string clientSecret = "MojioTestClientSecret";
+        const string vaultBaseUrl = "https://mojiotestkv.vault.azure.net/";
+        const string clientId = "a9ce8b9b-86c9-4bc1-8839-3a0dc5b654b5";
 
-        public async Task<string> GetKeyVaultInfo()
+        public class MyKeySecrets
         {
-             string url = "https://management.azure.com/subscriptions/" + subscriptionId + 
-                "/resourceGroups/" + resourceGroupName + 
-                "/providers/Microsoft.KeyVault/vaults/" + vaultName + 
-                "?api-version="+apiVersion;
-           
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", kvToken);
-            HttpResponseMessage response = await client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-            return responseBody;
+            public string SecretName { get; set; }
+            public string SecretContent { get; set; }
+
         }
 
-        // TODO : IMPLEMENT SCREEN TO CONNECT WITH USER ACCOUNT
-        public async Task<string> GetOAuthToken()
+        //the method that will be provided to the KeyVaultClient
+        public async Task<string> GetSecret(string secretId)
         {
-            string url = "https://login.microsoftonline.com/"+ directoryId + "/oauth2/authorize?" +
-                    "client_id = " + appId +
-                    "& response_type = code" +
-                    "& response_mode = query" +
-                    "& resource = " + uriId +
-                    "& state = 12345";
+            KeyVaultClient kvClient = AuthUsingADALCallback(vaultBaseUrl);
+            SecretBundle s = await kvClient.GetSecretAsync(vaultBaseUrl, secretId);
+            return s.Value;
+        }
 
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            string responseBody = response.Headers.GetValues("Location").First();
+        public void SetSecret(string secretId, string secret)
+        {
+            KeyVaultClient kvClient = AuthUsingADALCallback(vaultBaseUrl);
+            Task task = Task.Run(async () =>
+            {
+                await kvClient.SetSecretAsync(vaultBaseUrl, secretId, secret);
+            });
+        }
 
-            return responseBody;
+        public void UpdateSecret(string secretId, string secret)
+        {
+            KeyVaultClient kvClient = AuthUsingADALCallback(vaultBaseUrl);
+            Task task = Task.Run(async () =>
+            {
+                await kvClient.UpdateSecretAsync(vaultBaseUrl, secretId, secret);
+            });
+        }
 
+        private KeyVaultClient AuthUsingADALCallback(string vaultBaseURL)
+        {
+            // Set up a KV Client with an ADAL authentication callback function
+            KeyVaultClient kvClient = new KeyVaultClient(
+                async (string authority, string resource, string scope) =>
+                {
+                    var authContext = new AuthenticationContext(authority);
+                    ClientCredential clientCred = new ClientCredential(clientId, clientSecret);
+                    AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+                    if (result == null)
+                    {
+                        throw new InvalidOperationException("Failed to retrieve access token for Key Vault");
+                    }
+
+                    return result.AccessToken;
+                }
+            );
+
+            return kvClient;
         }
     }
 }
